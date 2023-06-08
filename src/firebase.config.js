5a94f73e3ca79b63e2de1b7b67f315538c041187
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-08 13:10:32 +0100

    Firebase config set up

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase-firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbl-tsOJfWQUcewa0Fwb94kBYvOYBrwHM",
+  authDomain: "house-market-place-app-57e96.firebaseapp.com",
+  projectId: "house-market-place-app-57e96",
+  storageBucket: "house-market-place-app-57e96.appspot.com",
+  messagingSenderId: "971355605515",
+  appId: "1:971355605515:web:9c442688bfb79a6826550e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
