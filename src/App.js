f9cf952c03fdc6761c83bbec852d7639e0a6a050
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-10 11:55:26 +0100

    Routing added to App. All routes tested and passed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div className="App">
-      My App
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Signin />} />
+        <Route path="/sign-in" element={<Signup />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/forgotten-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+    {/* Navbar */}
+    </>
   );
 }
 
