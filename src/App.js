ea0cdb5de4613e416f35b062886e539c865e759c
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-11 17:03:16 +0100

    Links added to Navbar

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
+// import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/offers" element={<Offers />} />
-        <Route path="/profile" element={<Signin />} />
-        <Route path="/sign-in" element={<Signup />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/forgotten-password" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
-    {/* Navbar */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Signin />} />
+          <Route path="/sign-in" element={<Signup />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgotten-password" element={<ForgotPassword />} />
+        </Routes>
+        {/* Navbar */}
+        <Navbar />
+      </Router>
     </>
   );
 }
