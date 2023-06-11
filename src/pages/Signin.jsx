7e06886400eeed1c5f4ff8b467ef6b28713cfdeb
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-11 18:09:04 +0100

    Sign up form created and styling and functionality added

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import VisibilityIcon from '../assets/svg/visibilityIcon.svg';
+
 
 function Signin() {
+  const [showPassword,setShowPassword] = useState(false);
+  const [formData,setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const navigate =  useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+      
+    }))
+  };
+
+
   return (
-    <div>Sign in</div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">
+            Welcome Back!
+          </p>
+        </header>
+        <form>
+          <input type="email" className="emailInput" placeholder='Email' id='email' value={email} onChange={onChange}/>
+          <div className="passwordInputDiv">
+            <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='Password' id='password' value={password} onChange={onChange} />
+            <img src={VisibilityIcon} alt="Show password" onClick={() => setShowPassword((prevState) => !prevState)}/>
+          </div>
+          <Link to='/forgotten-password' className='forgotPasswordLink'>Forgot Password</Link>
+          <div className="signInBar">
+            <p className="signInText">
+              Sign In
+            </p>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#fffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+        {/* Google OAuth */}
+
+        <Link to='/sign-up' className='registerLink'>Sign Up Instead</Link>
+      </div>
+    </>
+
   )
 }
 
