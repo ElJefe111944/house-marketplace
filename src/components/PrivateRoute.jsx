853b8ccf4bd9e456f1b95135dae412e20a9d9654
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-14 08:18:19 +0100

    useAuthStatus hook created and added to PrivateRoute component. Will check to see if the user is currently logged in

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,13 @@
-import { Navigate, Outlet } from "react-router"
+import { Navigate, Outlet } from "react-router";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 
 const PrivateRoute = () => {
 
-    const loggedIn = false;
+    const { loggedIn, checkingStatus } = useAuthStatus();
+
+    if(checkingStatus){
+        return <h3>Loading...</h3>
+    }
 
 
   return loggedIn ? <Outlet /> : <Navigate to='/sign-in/' />
