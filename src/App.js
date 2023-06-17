6bd737bbcf22c2dd4e2be256ae4fc6bdd7d634a5
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-17 22:43:47 +0100

    Createlisting page created and added to router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
 import Category from "./pages/Category";
+import CreateListing from "./pages/CreateListing";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/forgotten-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         {/* Navbar */}
         <Navbar />
