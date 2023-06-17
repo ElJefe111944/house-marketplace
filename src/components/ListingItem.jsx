74afc454fe1bc879d0f45d4a909b72af7eb9a13a
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-06-17 22:30:31 +0100

    All info added to Listing item template

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,7 +5,7 @@ import BedIcon from '../assets/svg/bedIcon.svg';
 import BathTubIcon from '../assets/svg/bathtubIcon.svg';
 
 
-function ListingItem({ listing, id }) {
+function ListingItem({ listing, id, onDelete }) {
   return (
     <li className='categoryListing'>
         <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
@@ -22,8 +22,21 @@ function ListingItem({ listing, id }) {
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             {listing.type === 'rent' && ' / Month'}
           </p>
+          <div className="categoryListingInfoDiv">
+            <img src={BedIcon} alt="Bed" />
+            <p className="categoryListingInfoText">
+              {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
+            </p>
+            <img src={BathTubIcon} alt="Bath" />
+            <p className="categoryListingInfoText">
+               {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}
+            </p>
+          </div>
             </div>
         </Link>
+        {onDelete && (
+          <DeleteIcon className='removeIcon' fill='rgb(231,76,60)' onClick={() => onDelete(listing.id,listing.name)} />
+        )}
     </li>
   )
 }
